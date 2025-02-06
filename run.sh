ec9a243ed06f47f45cd5b0bf213e2dd92e7f8cac
Soufiane <safilo2016@gmail.com>
2025-02-06 18:03:37 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -10,14 +10,14 @@
 run() {
     echo "Running evaluation ..."
 
-    # # Check if virtual environment exists
-    # if [ ! -d ".venv" ]; then
-    #     echo "Virtual environment not found! Creating one..."
-    #     python3.12 -m venv .venv
-    # fi
+    # Check if virtual environment exists
+    if [ ! -d ".venv" ]; then
+        echo "Virtual environment not found! Creating one..."
+        python3.12 -m venv .venv
+    fi
 
-    # # Activate virtual environment
-    # source .venv/bin/activate
+    # Activate virtual environment
+    source .venv/bin/activate
 
     # Ensure required packages are installed
     pip install -r requirements.txt --quiet
